15512f9430890a7e9598d2f04910cd068501de77
CarbonAnik <shkhnk@gmail.com>
2021-10-17 15:07:56 +0600

    error test

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,17 +4,25 @@ import com.example.db.DB
 import com.example.routing.initializeRouting
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.litote.kmongo.json
 
 fun main() {
     embeddedServer(Netty, port = System.getenv("PORT").toInt() ) { //todo System.getenv("PORT").toInt()
         install(ContentNegotiation) { json() }
         val db = DB
         initializeRouting()
+        install(StatusPages){
+            exception<Throwable> { cause ->
+                call.respond(cause.json)
+            }
+        }
     }.start(wait = true)
 }
--- src/main/kotlin/com/example/routing/authRouting.kt
+++ src/main/kotlin/com/example/routing/authRouting.kt
@@ -7,18 +7,17 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.litote.kmongo.json
 
-fun Route.authRouting(){
+fun Route.authRouting() {
     val col = DB.user
-    route("/signup"){
+    route("/signup") {
         post {
-            try {
-                val user = call.receive<User>()
-                call.respond(user)
-            } catch (e: Exception){
-                call.respond(e.json)
-            }
+//            try {
+            val user = call.receive<User>()
+            call.respond(user)
+//            } catch (e: Exception){
+//                call.respond(e.json)
+//            }
 //            if (col.insertOne(user).wasAcknowledged()){
 //                call.respondText("User Successfully Created", status = HttpStatusCode.Created)
 //            } else {
@@ -27,10 +26,10 @@ fun Route.authRouting(){
         }
     }
 
-    route("/login"){
+    route("/login") {
         post {
             val authRequest = call.receive<AuthRequest>()
-            if (col.insertOne(authRequest.user).wasAcknowledged()){
+            if (col.insertOne(authRequest.user).wasAcknowledged()) {
                 call.respondText("User stored correctly", status = HttpStatusCode.Created)
             } else {
                 call.respondText("User not correctly stored", status = HttpStatusCode.BadRequest)
